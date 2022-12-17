b397ee64f46936024e87ab659460d0517fef1ec3
dardaelliande <74540538+dardaelliande@users.noreply.github.com>
2022-12-17 15:47:55 +0100

    V2 sondage
    Prend désormais en compte les questions à choix multiple

--- src/js/sondage.js
+++ src/js/sondage.js
@@ -1,77 +1,94 @@
-const survey = {
-    "Merci d'entrer ici votre pseudonyme" : [],
-    "Etes-vous un homme ou une femme?": ["Homme", "Femme", "Autre", "Je ne souhaite pas répondre"],
-    "Dans quelles tranches d'âges vous situez-vous?": ["Moins de 20 ans", "entre 21 et 35 ans", "entre 36 et 60 ans", "plus de 60 ans"],
-    "Quel est votre profession": ["Etudiant(e)", "Fonction publique", "Fonction semi-publique", "Salarié en privé", "Autre"],
-    "Dans quelles tranches de salaires vous vous trouvez?": ["Moins de 500 dt", "Entre 500 dt et 1000dt", "Entre 1000dt et 1500 dt", "plus de 1500dt"],
+const survey = [{
+  type: 'multiple',
+  question: 'Quel est votre animal préféré ?',
+  choices: ['Chat', 'Chien', 'Oiseau', 'Autre'],
+},
+{
+  type: 'unique',
+  question: 'Quel est votre film préféré ?',
+  choices: ['Le Parrain', 'Retour vers le futur', 'Forrest Gump', 'Autre'],
+},
+{
+  type: 'text',
+  question: 'Quel est votre plat préféré ?',
+},
+];
 
-    "Quel est votre origine sociale?": [],
-    "Achetez-vous du yaourt?": ["Oui", "Non"],
-    "Parmi les marques du yaourt ci dessous,les quelles connaissez-vous?": ["Vitalait", "Délice danone", "Still", "Yogo", "Yab", "Nestlé"],
-    "Quel est votre marque préféré?": [],
-    "A quelle fréquence consommez-vous des yaourts?": ["Une fois par jour", "Deux fois par jour", "Au moins une fois par semaine", "Au moins cinq fois par semaine"],
-  };
-  /*
-  for (const question in survey) {
-    console.log(question);
-    for (const answer of survey[question]) {
-      console.log(`- ${answer}`);
-    }
+console.log(survey);
+
+function displaySurvey() {
+const surveyContainer = document.querySelector('#survey');
+survey.forEach((question) => {
+  // Créer un élément div pour chaque question
+  const questionContainer = document.createElement('div');
+  questionContainer.classList.add('question-container');
+  // Ajouter la question au div
+  const questionElement = document.createElement('p');
+  questionElement.innerText = question.question;
+  questionContainer.appendChild(questionElement);
+  // Ajouter les réponses possibles au div
+  if (question.type === 'multiple' || question.type === 'unique') {
+    question.choices.forEach((choice) => {
+      const choiceContainer = document.createElement('div');
+      choiceContainer.classList.add('choice-container');
+      // Créer un élément input selon le type de question
+      const inputElement = document.createElement('input');
+      inputElement.type = (question.type == 'multiple') ? 'checkbox' : 'radio';
+      inputElement.name = question.question;
+      inputElement.value = choice;
+      choiceContainer.appendChild(inputElement);
+      // Ajouter le texte de la réponse à côté de l'input
+      const choiceText = document.createElement('span');
+      choiceText.innerText = choice;
+      choiceContainer.appendChild(choiceText);
+      questionContainer.appendChild(choiceContainer);
+    });
+  } else if (question.type === 'text') {
+    const choiceContainer = document.createElement('div');
+    choiceContainer.classList.add('choice-container');
+    // Créer un élément input de type texte pour les questions textuelles
+    const inputElement = document.createElement('input');
+    inputElement.type = 'text';
+    inputElement.name = question.question;
+    choiceContainer.appendChild(inputElement);
+    questionContainer.appendChild(choiceContainer);
   }
-  */
 
-  const surveyDiv = document.querySelector("#survey");
-  for (const question in survey) {
-    const questionElement = document.createElement("p");
-    questionElement.textContent = question;
-    questionElement.classList.add("question");
-    
-    surveyDiv.appendChild(questionElement);
+  
 
-    if(survey[question].length==0){
-      const label = document.createElement("label");
-      const input = document.createElement("input");
-      input.classList.add("formInput");
-      input.type = "input";
-      input.name = question;
-      label.appendChild(input);
-      label.appendChild(document.createTextNode(survey[question]));
-      surveyDiv.appendChild(label);
-    }
 
-    for (const answer of survey[question]) {
-      const label = document.createElement("label");
-      const radio = document.createElement("input");
-      
-      radio.type = "radio";
-      radio.name = question;
-      radio.value = answer;
-      label.appendChild(radio);
-      label.appendChild(document.createTextNode(answer));
-      surveyDiv.appendChild(label);
-    }
-  }
-  const submitButton = document.createElement("button");
-  submitButton.textContent = "Soumettre";
-  submitButton.addEventListener("click", () => {
-    const responses = {};
-for (const question in survey) {
-  if (survey[question].length === 0) {
-    // Récupérer la valeur de l'élément de formulaire de type input
-    const response = document.querySelector(`input[name="${question}"]`);
-    if (response) {
-      responses[question] = response.value;
-    }
-  } else {
-    // Récupérer la valeur de l'élément de formulaire de type radio
-    const response = document.querySelector(`input[name="${question}"]:checked`);
-    if (response) {
-      responses[question] = response.value;
-    }
-  }
+  // Ajouter le div de la question à la div principale du sondage
+  surveyContainer.appendChild(questionContainer);
+});
+
+const submitButton = document.createElement('button');
+  submitButton.innerText = 'Envoyer';
+  submitButton.addEventListener('click', function() {
+    const answers = {};
+
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach((input) => {
+      if (input.checked) {
+        // Si la question existe déjà dans l'objet answers, ajouter la réponse au tableau
+        if (answers[input.name]) {
+          answers[input.name].push(input.value);
+        } else {
+          // Sinon, créer un tableau avec la réponse
+          answers[input.name] = [input.value];
+        }
+      } else if (input.type === 'text') {
+        // Pour les questions textuelles, stocker la réponse comme une chaîne de caractères
+        answers[input.name] = input.value;
+      }
+    });
+
+    console.log(answers);
+  });
+
+
+  surveyContainer.appendChild(submitButton);
+
 }
 
-    console.log(responses);
-    // Envoyer les réponses au serveur ici...
-  });
-  surveyDiv.appendChild(submitButton);
+// Appeler la fonction pour afficher le sondage
+displaySurvey();
